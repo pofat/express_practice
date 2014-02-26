2a84757e673222ccc5ddf6175eea58f1f8f90cb2
Lin Cheng Kai <whitefur@domiso.cc>
2014-02-26 17:39:37 +0800

    express practice

---
+++ server.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+
+console.log('Server running');
+app.get('/', function(req, res){
+	res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/send', function(req, res){
+	var message = '<h1>Welcome to Express Server</h1>';
+	var name = req.query.name;
+	var age = req.query.age;
+
+	if(name)
+		message = message + '<p>Hello ' + name + '</p>';
+	if(age)
+		message = message + '<p>You are ' + age + ' years old.</p>';
+	
+	res.send(message);
+	res.end();
+});
+
+app.use(express.bodyParser());
+app.post('/formData', function(req, res){
+	console.log(req.body);
+	res.send('<h1>Hello ' + req.body.username + '</h1>');
+	res.end();
+});
+
+app.listen(8888);
